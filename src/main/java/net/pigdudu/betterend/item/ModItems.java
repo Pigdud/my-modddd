d44daabc7f50f17e199a8117ca5f2144c78009cd
Pigdud <brucelopig@gmail.com>
2025-01-22 14:35:50 +0800

    hi

--- src/main/java/net/pigdudu/betterend/item/ModItems.java
+++ src/main/java/net/pigdudu/betterend/item/ModItems.java
@@ -1,6 +1,29 @@
 package net.pigdudu.betterend.item;
 
-public class ModItems {
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.pigdudu.betterend.Betterend;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Betterend.MOD_ID,"pink_garnet")))));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Betterend.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        Betterend.LOGGER.info("Registering Mod Items for " + Betterend.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+    }
 
 }
